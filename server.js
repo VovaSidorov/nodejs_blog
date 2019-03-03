5a5a326c926e6c8e7c04f79c35f25cd7ed9ee57b
VovaSidorov <VovaCivic@gmail.com>
2019-03-03 15:48:18 +0200

    add route for show user

--- server.js
+++ server.js
@@ -23,10 +23,16 @@ connection.query('CREATE TABLE IF NOT EXISTS users(' +
 
 app.get('/users',(req,res)=>{
 
-    console.log(req.query);
-
+    const {query} = req;
     let limitRow = 4;
     let offset = 0;
+    let page = 1;
+
+    console.log(query);
+    if ('page' in query && Number.isInteger(parseInt(query.page))&& parseInt(query.page)>0){
+        page = parseInt(query.page);
+             offset = limitRow*(query.page-1)
+    }
 
     const query1 = `select * from users LIMIT ${limitRow} OFFSET ${offset};`;
     const query2 = `select count(*) as count from users;`;
@@ -41,6 +47,29 @@ app.get('/users',(req,res)=>{
             status: "ok"
         });
     })
+});
+
+app.get('/users/:id',(req, res) => {
+
+    const {params: {id}} = req;
+    console.log(id);
+
+    if(Number.isInteger(parseInt(id)) && parseInt(id) > 0){
+        const query1 = `select * from users WHERE id = ${id};`;
+        connection.query(query1, (err, result) => {
+            if(err){}
+            console.log(result);
+            res.send({
+                data: result,
+                status: "ok"
+            });
+        })
+    }else {
+        res.send({
+            status: "error",
+            error: "Error get data"
+        });
+    }
 });
 
 app.listen("3000", err => {
