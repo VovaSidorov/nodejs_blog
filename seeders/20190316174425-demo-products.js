d1051835e9fab0eece3385d50b839bb934f54413
VovaSidorov <VovaCivic@gmail.com>
2019-03-16 21:54:23 +0200

    Create Cart seeder

--- seeders/20190316174425-demo-products.js
+++ seeders/20190316174425-demo-products.js
@@ -31,12 +31,12 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      // Add reverting commands here.
+      // Return a promise to correctly handle asynchronicity.
+      //
+      // Example:
+      return queryInterface.bulkDelete('Products', null, {});
+
   }
 };
